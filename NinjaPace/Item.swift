873acdcdb4e38e5c1d5c741e3196d6e9f3307f7d
Kevin Montemayor <k.montemayor1@gmail.com>
2026-02-04 18:29:48 -0700

    Initial Commit

---
+++ NinjaPace/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  NinjaPace
+//
+//  Created by Kevin Montemayor on 2/4/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ NinjaPace/NinjaPaceApp.swift
@@ -0,0 +1,32 @@
+//
+//  NinjaPaceApp.swift
+//  NinjaPace
+//
+//  Created by Kevin Montemayor on 2/4/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NinjaPaceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
